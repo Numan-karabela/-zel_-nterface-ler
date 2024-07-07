fb084ea734f2671bd5075359ad886fe805e83a99
1453n <1453n@DESKTOP-PHGFDVP>
2024-07-07 19:25:39 +0300

    CompareTo

---
+++ CompareTo/Program.cs
@@ -0,0 +1,16 @@
+﻿Person person1 = new Person() { Name = "Mehmet", Age = 28 };
+Person person2 = new Person() { Name = "Temel", Age = 25 };
+
+
+Console.WriteLine(person1.CompareTo(person2));
+
+public class Person : IComparable<Person>
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public int CompareTo(Person? other)
+    {
+        return other.Age.CompareTo(Age);
+    }
+}
